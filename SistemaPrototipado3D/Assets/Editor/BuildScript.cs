75da0ec010e317e288bf1b97bdc563ff47660ac1
Exequiel Pagliari <exepa@hotmail.com>
2025-05-23 16:48:48 -0300

    - Cambio de BuildScript

--- SistemaPrototipado3D/Assets/Editor/BuildScript.cs
+++ SistemaPrototipado3D/Assets/Editor/BuildScript.cs
@@ -4,7 +4,7 @@ public class BuildScript
 {
     public static void BuildGame()
     {
-        string[] scenes = { "Assets/Scenes/MainScene.unity" };
+        string[] scenes = { "Assets/Scenes/PlayDemo.unity" };
         BuildPipeline.BuildPlayer(scenes, "Builds/Windows/MyGame.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 }
